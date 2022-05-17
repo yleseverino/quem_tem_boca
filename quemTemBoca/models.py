4e08e1564ea76b196383686adf39e06b45ee8c6b
yleseverino <ylewap0@gmail.com>
2022-05-17 16:25:15 -0300

    add url images fields to Restaurante model

--- quemTemBoca/models.py
+++ quemTemBoca/models.py
@@ -1,18 +1,33 @@
-from pydantic import BaseModel, ValidationError, validator
+from pydantic import BaseModel, validator, Field, condecimal, HttpUrl
 from datetime import datetime
 
+from quemTemBoca.s3 import get_restaurant_logo, get_restaurant_background
 
 class Restaurante(BaseModel):
-    nome: str
-    descricao: str
-    pontuacao: float
+    id: int = Field(...)
+    nome: str = Field(...)
+    descricao: str = Field(...)
+    criado_em: datetime = datetime.now()
 
-    @validator('pontuacao')
-    def pontuacao_between_0_and_5(cls, v):
-        if v > 5:
-            raise ValueError('A pontuação de um restaurante não pode ser maior que 5') 
-        elif v < 0:
-            raise ValueError('A pontuação de um restaurante não pode ser menor que 0') 
+    logo_url : HttpUrl
+    background_url : HttpUrl
+
+    "A pontuação deve ser maior que 0, ter 2 casas decimais e ser menor que 5"
+    pontuacao: condecimal(decimal_places=2, ge=0, le=5)
+
+    "O preco de entrega deve ser menor or igual 100 reais e maior que 0 reais"
+    preco_entrega: condecimal( decimal_places=2, ge=0, le=100)
+    
+    @validator('nome')
+    def nome_menor_que_255(cls, v):
+        if len(v) > 255:
+            raise ValueError('O numero de caracteres no nome não pode ser maior que 255') 
+        return v
+    
+    @validator('descricao')
+    def descricao_menor_que_1000(cls, v):
+        if len(v) > 1000:
+            raise ValueError('O numero de caracteres no nome não pode ser maior que 255') 
         return v
 
-    criado_em: datetime = datetime.now()
+    
