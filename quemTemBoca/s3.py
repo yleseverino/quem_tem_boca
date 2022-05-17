d543da99528988ec687870920dc461aafdf5506f
yleseverino <ylewap0@gmail.com>
2022-05-17 17:39:21 -0300

    add method to get the dish img from s3

--- quemTemBoca/s3.py
+++ quemTemBoca/s3.py
@@ -14,3 +14,8 @@ def get_restaurant_background(restaurant_id):
                 return "https://cdn.pixabay.com/photo/2014/04/22/02/56/pizza-329523_960_720.jpg"
         return f'{ENDPOINT_URL}/{BUCKET}/restaurantes/{restaurant_id}/backgroud.webp?AWSAccessKeyId={AWS_ACCESS_KEY_ID}'
 
+def get_dishe_img(prato_id):
+        if not AWS_ACCESS_KEY_ID:
+                return "https://cdn.pixabay.com/photo/2016/10/05/05/46/bread-1716102_960_720.jpg"
+        return f'{ENDPOINT_URL}/{BUCKET}/pratos/{prato_id}.webp?AWSAccessKeyId={AWS_ACCESS_KEY_ID}'
+
